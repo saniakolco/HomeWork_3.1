9e84a570f79c2cb78e97c4e08e3b17e76fa95708
Александр Коваленко <aleksandrkovalenko@iMac-Aleksandr.local>
2022-11-06 16:41:13 +0400

    Save

---
+++ HomeWork_3.2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трёхзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.WriteLine("Третьего числа не существует");
+}
+else
+{
+    num = num % 100 / 10;
+}
+Console.WriteLine(num);
+
