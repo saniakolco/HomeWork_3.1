db0ce792a78347f93c2dabd92d81575ffda6e688
Александр Коваленко <aleksandrkovalenko@iMac-Aleksandr.local>
2022-11-06 14:53:44 +0400

    resolved

---
+++ HomeWork_3.3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число от 1го до 7 ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 6 || num == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Рабочий день");
+}
